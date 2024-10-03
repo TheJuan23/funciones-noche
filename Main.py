10b35309af76b80f652015fe194f441e4e36788f
TheJuan23 <pablocabrera.p21@gmail.com>
2024-10-03 18:53:46 -0500

    se crea el archivo

---
+++ Main.py
@@ -0,0 +1,8 @@
+def my_funcion():
+    print("hello from funcion")
+my_funcion()
+#ejemplo funcion con argumento
+def mostrarNombre(nombre):
+    print("Su nombre es: "+nombre)
+nom = input("ingresa tu nombre: ")
+mostrarNombre(nom)
